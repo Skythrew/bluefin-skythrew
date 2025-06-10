9adcad73f682198db3d4955a41775c592d3fe7fc
Maël GUERIN <mael.guerin@murena.io>
2025-06-10 16:30:45 +0200

    feat: add steam native package

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,5 +2,9 @@
 
 set -ouex pipefail
 
+# Canon Printer Driver
 dnf install -y /ctx/rpm/cups-drivers-bjnp-2.0.3-2.mga9.x86_64.rpm
 
+# Steam
+dnf5 -y config-manager addrepo --from-repofile=https://negativo17.org/repos/fedora-steam.repo
+dnf install -y steam
